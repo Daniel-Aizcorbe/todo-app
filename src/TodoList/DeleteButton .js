ca70673902968a0c462d0f5dd33dd6bcf88250dc
daniel-aizcorbe <daizcorbe@frba.utn.edu.ar>
2023-08-28 03:27:32 -0300

    add DeleteButton

---
+++ src/TodoList/DeleteButton .js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./DeleteButton.css";
+
+function DeleteButton() {
+
+    return (
+        <button className="delete-btn">
+            X
+        </button>
+    );
+    
+}
+
+export {DeleteButton}
--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { DeleteButton } from "./DeleteButton ";
 
 function TodoItem({text, completed}) {
 
     return (
         <li className="todo_item" key={text} value={text}>
             <p>{text} : {completed ? "V" : "F"}</p>
+            <DeleteButton />
         </li>
     );   
 }
